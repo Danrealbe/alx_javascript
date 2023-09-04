4b69da65b68ddd87eb3b3c03bda4999fe6054222
danrealbe <odeboy5518@gmail.com>
2023-09-04 08:08:04 -0700

    Javascript is amazing

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 /* prints “JavaScript is amazing”:
  * You must create a constant variable called myVar with the value “JavaScript is amazing”
  * You must use console.log(...) to print all output
