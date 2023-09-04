cca016c9934766ae3501d235a98e85a82055e87e
danrealbe <odeboy5518@gmail.com>
2023-09-04 07:50:08 -0700

    Javascript is amazing

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+/* prints “JavaScript is amazing”:
+ * You must create a constant variable called myVar with the value “JavaScript is amazing”
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var  */
+
+myVar = "Javascript is amazing"
+console.log(myVar)
