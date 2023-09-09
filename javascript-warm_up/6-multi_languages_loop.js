b75c14657780837d63d56ccf055296f86f9969bc
danrealbe <odeboy5518@gmail.com>
2023-09-09 05:54:25 -0700

    mutiple line in erray

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-for (const List of ['C is fun', 'Python is cool', 'JavaScript is amazing']); {
-	console.log(List); 
-} 
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
+}
